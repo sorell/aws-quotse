167b0bff640f312b4250523bc4c8b3e8515703f4
Sami Sorell <sami.sorell@vincit.fi>
2020-10-15 09:17:37 +0300

    random quote done

--- server.js
+++ server.js
@@ -6,25 +6,179 @@ const port = 8080;
 
 const querystring = require('querystring');
 
-var AWS = require("aws-sdk");
+var AWS = require('aws-sdk');
 
-AWS.config.update({
-  region: "eu-west-1",
-//  endpoint: "arn:aws:dynamodb:eu-west-1:045172193889:table/Quotse"
-});
+AWS.config.update({region: "eu-west-1"});
 
 var dynamodb = new AWS.DynamoDB();
 //var dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
-const server = http.createServer((req, res) => {
-  console.log(`Got ${req} ` + req.url + ' ')
-  for (var prop in req.headers) {
-    if (req.headers.hasOwnProperty(prop)) { console.log(prop + ': ' + req.headers[prop]) }
+
+function getQuoteCount(resultsFunc, res) {
+  const params = {
+    TableName: 'Counters',
+    Key: {
+      'Name' : {S: 'Quotes'}
+    }
+  };
+
+  dynamodb.getItem(params, function(err, data) {
+    if (err) {
+      console.error('getQuoteCount ', err);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Fail. Darnation!');
+    }
+    else {
+      console.log('GETCNTR ', data);
+      if (resultsFunc) {
+        resultsFunc(data.Item.Value.N, res);
+      }
+    }
+  });
+}
+
+
+function getQuoteNr(itemCount, res) {
+  console.log('itemCount', itemCount)
+
+  if (itemCount < 1) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Database empty');
+    return;
   }
+
+  const getIdx = Math.floor(Math.random() * itemCount);
+  const params = {
+    TableName: 'Quotse',
+    Key: {
+      'Idx': {N: getIdx.toString()}
+    }
+  };
+
+  dynamodb.getItem(params, function(err, data) {
+    if (err) {
+      console.error('getQuoteNr(', getIdx, '/', itemCount, ') error: ', err);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Fail. Darnation!');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end('{"response_type":"in_channel","text":"' + data.Item.Text.S.split('"').join('\\"') + '"}');
+    }
+  });
+}
+
+
+function queryQuote(keyword, res) {
+    const params = {
+      TableName: 'Quotse',
+      KeyConditionExpression: "#id = :idnum",
+      ExpressionAttributeNames:{"#id": "Idx"},
+      ExpressionAttributeValues: {":idnum": {'N': getIdx.toString()}}
+    }
+    console.log('QUERY', params)
+
+    dynamodb.query(params, function(err, data) {
+      if (err) {
+        console.log("Error", err)
+      } else {
+        console.log("Success", data.Items)
+        console.log(data)
+        data.Items.forEach(function(element, index, array) {
+          console.log(element.Idx.S + " (" + element.title.S + ")")
+        });
+      }
+    });
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end('{"response_type":"in_channel","text":"You say: WTF ' + text + '"}');
+}
+
+
+function pushQuotes(res) {
+  try {
+    var fs = require('fs');
+    const allLines = fs.readFileSync('qsuotes.txt', 'utf8');
+    const lines = allLines.split('\n');
+    var addedLines = 0;
+    lines.forEach(function(line) {
+      if (line.length > 1) {
+        const params = {
+          TableName: 'Quotse',
+          Item: {
+            'Idx': {N: addedLines.toString()},
+            'Text': {S: line}
+          }
+        };
+
+        dynamodb.putItem(params, function(err, data) {
+          if (err) {
+            console.error('Write error', err);
+          }
+        });
+
+        addedLines += 1;
+      }
+    });
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(addedLines.toString() + ' lines added');
+  }
+  catch (err) {
+    console.error(err);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Pffffff....');
+  }
+}
+
+
+function adminAction(text, res) {
+  console.log('adminAction', text);
+
+  const parts = text.split(' ');
+  if (parts.length < 2) {
+    console.log('adminAction parts.length =', parts.length.toString());
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Parameters missing');
+    return;
+  }
+
+  if (parts[0] != '2T0uVdwJTd2hSSx9oa') {
+    console.log('adminAction bad pass');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bad param');
+    return;
+  }
+
+  if (parts[1] == 'pushQuotes') {
+    pushQuotes(res);
+  }
+  else {
+    console.log('adminAction unk cmd');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bad param');
+  }
+}
+
+
+const server = http.createServer((req, res) => {
+  // console.log(`Got ${req} ` + req.url + ' ')
+  // for (var prop in req.headers) {
+  //   if (req.headers.hasOwnProperty(prop)) { console.log(prop + ': ' + req.headers[prop]) }
+  // }
 
   if (!req.headers.hasOwnProperty('user-agent')  ||  !req.headers['user-agent'].includes('Slackbot')  ||  !req.headers.hasOwnProperty('x-slack-signature')) {
     console.log('Request headers invalid');
-    res.statusCode = 400;
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Pffffff....');
     return;
@@ -42,77 +196,28 @@ const server = http.createServer((req, res) => {
       console.log('TEXT', text);
       var itemCount = 0;
 
-      if (text == 'init') {
-        var params = {
-          TableName: 'Counters',
-          Item: {
-            'Name' : {S: 'Quotes'},
-            'Value' : {N: '0'}
-          }
-        };        
-
-        dynamodb.putItem(params, function(err, data) {
-          if (err) {
-            console.log("Error", err);
-          } else {
-            console.log("Success", data);
-          }
-        });
+      if (text == 'get') {
       }
 
       if (action == 'getrandom') {
-        dynamodb.describeTable({TableName: 'Quotse'}, function(err, data) {
-          if (err) {
-            console.log("ERROR DESCRIBE", err);
-          } else {
-            console.log("SUCCESS DESCRIBE", data.Items);
-          }
-        });
-        //const description = await dynamodb.describeTable({TableName: 'Quotse'}).promise()
-
-
-console.log('COUNT', itemCount);
-        if (itemCount >= 0) {
-          var params = {
-            TableName: 'Quotse',
-            KeyConditionExpression: "#id = :idnum",
-            ExpressionAttributeNames:{"#id": "Idx"},
-//            ExpressionAttributeValues: {":idnum": {'N': Math.floor(Math.random() * itemCount).toString()}}
-            ExpressionAttributeValues: {":idnum": {'N': Math.floor(Math.random() * 4).toString()}}
-          };
-          console.log('QUERY');
-          console.log(params);
-
-          dynamodb.query(params, function(err, data) {
-            if (err) {
-              console.log("Error", err);
-            } else {
-              console.log("Success", data.Items);
-              console.log(data);
-              data.Items.forEach(function(element, index, array) {
-                console.log(element.Idx.S + " (" + element.title.S + ")");
-              });
-            }
-          });
-
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.end('{"response_type":"in_channel","text":"You say: ' + text + '"}');
-          return;
-        }
+        getQuoteCount(getQuoteNr, res);
+      }
+      if (action == 'add') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not implemented yet');
+      }
+      else if (action == 'admin') {
+        adminAction(text, res);
       }
     }
     catch (e) {
       console.error(e.message);
     }
-  }).on('error', () => {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Pffffff....');
   });
 
-  console.log('QUERY ');
-  console.log(require('querystring').parse(req.data));
+//  console.log('QUERY ');
+//  console.log(require('querystring').parse(req.data));
 });
 
 server.listen(port, hostname, () => {
